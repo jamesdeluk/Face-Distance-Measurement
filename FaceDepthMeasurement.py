b94d29071a1d1a7f166d322d286cf7635186bb1e
James <jgibbins92@gmail.com>
2024-04-29 08:06:21 +0100

    Update FaceDepthMeasurement.py

--- FaceDepthMeasurement.py
+++ FaceDepthMeasurement.py
@@ -11,34 +11,42 @@ while True:
     success, img = cap.read()
     img, faces = detector.findFaceMesh(img, draw=False)
 
+    # Sleep to slow meaasurement to save resources/power
+    time.sleep(0.2)
+
     if faces:
         face = faces[0]
         pointLeft = face[145]
         pointRight = face[374]
-        # Drawing
+
+        # Draw line between eyes (pupillary distance)
         # cv2.line(img, pointLeft, pointRight, (0, 200, 0), 3)
         # cv2.circle(img, pointLeft, 5, (255, 0, 255), cv2.FILLED)
         # cv2.circle(img, pointRight, 5, (255, 0, 255), cv2.FILLED)
+
         w, _ = detector.findDistance(pointLeft, pointRight)
         W = 6.3
 
-        # # Finding the Focal Length
+        # Find the focal length
         # d = 50
         # f = (w*d)/W
         # print(f)
 
-        # Finding distance
-        f = 700
+        # Find distance
+        f = 680
         d = (W * f) / w
         print(d)
 
-        if d < 50:
-            winsound.Beep(6000, 100)  # Beep at 440 Hz for 1000 milliseconds
-            time.sleep(0.1)
+        # Beep at 6000 Hz for 100 milliseconds if distance under 
+        limit = 48
+        if d < limit:
+            winsound.Beep(6000, 100)
 
+        # Draw text box with depth measurement
         # cvzone.putTextRect(img, f'Depth: {int(d)}cm',
         #                    (face[10][0] - 100, face[10][1] - 50),
         #                    scale=2)
 
+    # Display face (also required for finding the focal length)
     # cv2.imshow("Image", img)
     # cv2.waitKey(1)
